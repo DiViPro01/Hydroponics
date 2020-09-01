b4e6eba4c92157eca082545f6e2e699d05f5ea8c
DiViPro01 <43602201+DiViPro01@users.noreply.github.com>
2020-09-01 21:15:18 +0200

    Update lamp.py
    Update naar laatste werkende programma. Functionaliteit van het wegschrijven naar een logbestand bijgestoken

--- lamp.py
+++ lamp.py
@@ -5,17 +5,36 @@ import RPi.GPIO as GPIO
 from datetime import datetime
 
 GPIO.setmode(GPIO.BCM)
-RELAIS_1_GPIO = 18
+RELAIS_1_GPIO = 27
 GPIO.setup(RELAIS_1_GPIO, GPIO.OUT)
 
-flag = "False"
-while flag == "False":
+loop = "loop"
+LampToestand="False"
+
+while loop == "loop" :
   DateNow = datetime.now()
   uur = DateNow.hour
-  if uur == 15 :  # lamp aan om 07 smorgens
-      GPIO.output(RELAIS_1_GPIO, GPIO.HIGH) # on
-  elif uur == 16 : # lamp uit om 01 smorgens
-      GPIO.output(RELAIS_1_GPIO, GPIO.LOW) # of
-  else :
-          print("doe niets")
 
+  if uur >= 8 and uur <= 24 :  # lamp aan om 08
+      GPIO.output(RELAIS_1_GPIO, GPIO.HIGH) # aan
+      if LampToestand != "Aan" :
+       LampToestand = "Aan"
+       f= open("/home/pi/logs/TriggerEvents.txt","a+")
+       f.write(datetime.now().ctime() + "  Lamp  " +  LampToestand +  "\n")
+       f.close()
+
+  elif uur >= 1 and uur <= 7 : # lamp uit om 01
+      GPIO.output(RELAIS_1_GPIO, GPIO.LOW) # uit
+      if LampToestand != "Uit" :
+        LampToestand = "Uit"
+        f= open("/home/pi/logs/TriggerEvents.txt","a+")
+        f.write(datetime.now().ctime() + "  Lamp  " + LampToestand +  "\n")
+        f.close()
+
+  else :
+        f= open("/home/pi/logs/TriggerEvents.txt","a+")
+        f.write(datetime.now().ctime() + "  FOUT IN SCRIPT LAMP " +  "\n")
+        f.close()
+
+
+  time.sleep(1800)
